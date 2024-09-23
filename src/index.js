6417b4461bec3aca9da422503a3ee824f32bac3e
Felipe Bucherenick <felipebucherenick@gmail.com>
2024-09-23 19:14:54 -0300

    feat:Create de classes for Projects, Todos and Notes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-const hello = document.createElement("h1");
-hello.textContent = "Hello World";
+const todoList = [];
 
-const content = document.getElementById("content");
-content.appendChild(hello);
+class Note {
+  constructor(content, isChecked) {
+    this.content = content;
+    this.isChecked = isChecked;
+  }
+  checkNote() {
+    this.isChecked = true;
+  }
+}
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = [];
+  }
+  addNote(newNote) {
+    this.notes.push(newNote);
+  }
+}
+
+class Project extends Todo {
+  constructor() {
+    super();
+    this.projectTodos = [];
+  }
+  addTodo(newTodo) {
+    this.projectTodos.push(newTodo);
+  }
+}
